590337ebb9113dc52d41a0795cdf0c29d9804856
Locke <moyerock@gmail.com>
2017-01-07 00:25:46 +0800

    refactor: sub-processing

--- chatper1/contextual-definition.rkt
+++ chatper1/contextual-definition.rkt
@@ -1,21 +1,16 @@
-(define (square x) (* x x))
-
-(define (sqrt-iter guess x)
-  (if (good-enough? guess x)
-      guess
-      (sqrt-iter (improve guess x)
-                 x)))
-
-(define (improve guess x)
-  (average guess (/ x guess)))
-
-(define (average x y)
-  (/ (+ x y) 2))
-
-
-(define  (good-enough? guess x)
-  (< (abs (- (square guess) x)) 0.001))
-
-(define (sqrt x) (sqrt-iter 1.0 x))
+(define (square x)
+    (define (sqrt-iter guess x)
+      (if (good-enough? guess x)
+          guess
+          (sqrt-iter (improve guess x)
+                     x)))
+    (define (improve guess x)
+      (average guess (/ x guess)))
+    (define (average x y)
+      (/ (+ x y) 2))
+    (define  (good-enough? guess x)
+      (< (abs (- (square guess) x)) 0.001))
+    (define (sqrt x) (sqrt-iter 1.0 x))
+    (* x x))
 
 (display (sqrt 2))
--- chatper1/practice/p1-8.rkt
+++ chatper1/practice/p1-8.rkt
@@ -1,23 +1,18 @@
 #lang racket
 
-(define (cube x) (* x x x))
-
-(define (square x) (* x x))
-
-(define (cube-root-iter guess x)
-  (if (good-enough? guess x)
-      guess
-      (cube-root-iter (improve guess x)
-                 x)))
-
-(define (improve guess x)
-  (/ (+ (/ x (square guess)) (* 2 guess))
-     3))
-
-
-(define (good-enough? guess x)
-  (< (abs (- (cube guess) x)) 0.001))
-
-(define (cube-root x) (cube-root-iter 1.0 x))
+(define (cube-root x)
+  (define (cube x) (* x x x))
+  (define (square x) (* x x))
+  (define (cube-root-iter guess x)
+    (if (good-enough? guess x)
+        guess
+        (cube-root-iter (improve guess x)
+                        x)))
+  (define (improve guess x)
+    (/ (+ (/ x (square guess)) (* 2 guess))
+       3))
+  (define (good-enough? guess x)
+    (< (abs (- (cube guess) x)) 0.001))
+  (cube-root-iter 1.0 x))
 
 (cube-root 27)
