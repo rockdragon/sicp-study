636f468ab32ef84434bd17ec88a782020a4a2be6
Locke <moyerock@gmail.com>
2017-01-11 00:28:24 +0800

    chore: O(log(n)) exponential

--- chatper1/exponential.rkt
+++ chatper1/exponential.rkt
@@ -1,4 +1,4 @@
-; 递归算幂
+; 线性递归算幂
 (define (fast-exp m n)
   (define (fast-exp-iter m n acc)
     (cond ((= n 0) 1)
@@ -7,3 +7,12 @@
           ))
   (fast-exp-iter m n 1)
 )
+
+; O(Log(n))次 递归算幂
+(define (fast-expt b n)
+  (define (square x) (* x x))
+  (cond ((= n 0) 1)
+        ((even? n) (square (fast-expt  b (/ n 2))))
+        (else (* b (fast-expt b (- n 1))))))
+
+(fast-expt 6 3)
