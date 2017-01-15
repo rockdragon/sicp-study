18ccf5257dbc0edc1ca9627d2ff64da1f6f4c158
Locke <moyerock@gmail.com>
2017-01-15 18:59:11 +0800

    chore: prime test

--- chatper1/exponential.rkt
+++ chatper1/exponential.rkt
@@ -1,11 +1,11 @@
 ; 线性迭代算幂
-(define (fast-exp m n)
-  (define (fast-exp-iter m n acc)
+(define (linear-exp m n)
+  (define (linear-exp-iter m n acc)
     (cond ((= n 0) 1)
           ((= n 1) (* acc m))
-          (else (fast-exp-iter m (- n 1) (* acc m)))
+          (else (linear-exp-iter m (- n 1) (* acc m)))
           ))
-  (fast-exp-iter m n 1)
+  (linear-exp-iter m n 1)
 )
 
 ; O(Log(n))次 递归算幂
